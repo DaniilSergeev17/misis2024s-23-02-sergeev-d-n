3dedc5747edeaf622c3a269a9fdd1f612ee5eccf
DaniilSergeev17 <81472094+DaniilSergeev17@users.noreply.github.com>
2024-06-24 18:58:06 +0300

    Update 1873d.cpp

--- prj.codeforces/1873d.cpp
+++ prj.codeforces/1873d.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, k, point=0, res=0;
+        std::cin >> n >> k;
+        std::string st;
+        std::cin >> st;
+        for (int j = 0; j < n; j++) {
+            char pp = st[j];
+            if (pp == 'B') {
+                if (point == 0) {
+                    point = k - 1;
+                    if (point == 0) {
+                        res += 1;
+                    }
+                }
+                else if (point != 0) {
+                    if (point == 1) {
+                        point = 0;
+                        res += 1;
+                    }
+                    else {
+                        point -= 1;
+                    }
+                }
+            }
+            else if (point != 0) {
+                if (point == 1) {
+                    point = 0;
+                    res += 1;
+                }
+                else {
+                    point -= 1;
+                }
+            }
+        }
+        if (point != 0) {
+            res += 1;
+        }
+        std::cout << res << "\n";
+    }
+
+}
