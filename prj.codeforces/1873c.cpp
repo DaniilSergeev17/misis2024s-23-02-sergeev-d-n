8ffd5940e90c12c8eb224695e9fa873e2ef26d50
DaniilSergeev17 <81472094+DaniilSergeev17@users.noreply.github.com>
2024-06-24 19:00:09 +0300

    Update 1873c.cpp

--- prj.codeforces/1873c.cpp
+++ prj.codeforces/1873c.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
 
+
+int main() {
+	std::map<int, int> myDictionary;
+	int point_1 = 1, point_2 = 1, t;
+	std::cin >> t;
+	myDictionary[0] = 1;
+	myDictionary[1] = 2;
+	myDictionary[2] = 3;
+	myDictionary[3] = 4;
+	myDictionary[4] = 5;
+	myDictionary[5] = 5;
+	myDictionary[6] = 4;
+	myDictionary[7] = 3;
+	myDictionary[8] = 2;
+	myDictionary[9] = 1;
+
+	const int rows = 10;
+	const int cols = 10;
+
+	for (int z = 0; z < t; z++) {
+		int k = 0;
+		for (int i = 0; i < rows; ++i) {
+			std::string st;
+			std::cin >> st;
+			for (int j = 0; j < cols; ++j) {
+				if (st[j] == 'X') {
+					point_1 = myDictionary[i];
+					point_2 = myDictionary[j];
+					k += std::min(point_1, point_2);
+				}
+
+			}
+		}
+		std::cout << k<<'\n';
+	}
+	
+}
